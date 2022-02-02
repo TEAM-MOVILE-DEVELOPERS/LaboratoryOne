1d064c5c06efd72e0948bc40edb548e9d22dc6fe
Juan David Rendon Berrio <juan.rendon13@udea.edu.co>
2022-02-01 22:42:22 -0500

    Se crea el package de Ui y dentro de este los packages de login, main, register y splash con cada una de sus actividades, para que el grupo inicie el desarrollo de las mismas.

---
+++ app/src/main/java/com/juancho1037/umadefoods/ui/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.juancho1037.umadefoods.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.juancho1037.umadefoods.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_activity)
+    }
+}
---
+++ app/src/main/java/com/juancho1037/umadefoods/ui/register/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.juancho1037.umadefoods.ui.register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.juancho1037.umadefoods.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.register_activity)
+    }
+}
