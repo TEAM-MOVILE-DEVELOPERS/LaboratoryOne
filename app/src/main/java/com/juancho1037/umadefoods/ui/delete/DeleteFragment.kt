c53be5cceca7db28a6b83a0a7e86bd666eca07ea
Camilo Hernández Ruiz <camihruiz24@gmail.com>
2022-03-06 08:47:48 -0500

    Creado el Bottom Navigation Activity

---
+++ app/src/main/java/com/juancho1037/umadefoods/ui/delete/DeleteFragment.kt
@@ -0,0 +1,25 @@
+package com.juancho1037.umadefoods.ui.delete
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.juancho1037.umadefoods.R
+
+class DeleteFragment : Fragment() {
+	
+	companion object {
+		fun newInstance() = DeleteFragment()
+	}
+	
+	private lateinit var viewModel: DeleteViewModel
+	
+	override fun onCreateView(
+		inflater: LayoutInflater , container: ViewGroup? ,
+		savedInstanceState: Bundle?
+	): View? {
+		return inflater.inflate(R.layout.fragment_delete , container , false)
+	}
+}
---
+++ app/src/main/java/com/juancho1037/umadefoods/ui/delete/DeleteViewModel.kt
@@ -0,0 +1,7 @@
+package com.juancho1037.umadefoods.ui.delete
+
+import androidx.lifecycle.ViewModel
+
+class DeleteViewModel : ViewModel() {
+	// TODO: Implement the ViewModel
+}
